cf1b17a7722e727508d55d09e1bfb77a142d5d94
gamesplanet13 <gamesplanet.7983624797@gmail.com>
2026-09-13 22:48:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="games-planet-live-v32";
+const CACHE_NAME="games-planet-live-v34";
 const APP_SHELL=[
   "./","./index.html","./Simple.html","./order-prefill.html","./bigship-rate-calculator.html","./delhivery-config.js","./invoice.html","./manifest.webmanifest",
   "./assets/html2canvas.min.js",
